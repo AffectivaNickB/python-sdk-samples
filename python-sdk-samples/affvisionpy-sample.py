cd21a0abe652fc923046789c478f6211d5a9c9f5
AffectivaNickB <nick.bonfatti@affectiva.com>
2019-12-26 16:09:39 -0500

    Update affvisionpy-sample.py
    avoid a situation where num_faces could be undefined

--- python-sdk-samples/affvisionpy-sample.py
+++ python-sdk-samples/affvisionpy-sample.py
@@ -5,13 +5,13 @@ import sys
 import os
 import time
 from collections import defaultdict
-
+ 
 import affvisionpy as af
 import cv2 as cv2
 import math
-
-
-
+ 
+ 
+ 
 # Constants
 NOT_A_NUMBER = 'nan'
 count = 0
@@ -23,38 +23,39 @@ DEFAULT_FRAME_WIDTH = 1280
 DEFAULT_FRAME_HEIGHT = 720
 DEFAULT_FILE_NAME = "default"
 DATA_DIR_ENV_VAR = "AFFECTIVA_VISION_DATA_DIR"
-
+ 
 #Argparse Variable Constants
 WIDTH = 0
 HEIGHT = 1
-
+ 
 process_last_ts = 0.0
 capture_last_ts = 0.0
-
-
-
+ 
+ 
+ 
 header_row = ['TimeStamp', 'faceId', 'upperLeftX', 'upperLeftY', 'lowerRightX', 'lowerRightY', 'confidence', 'interocular_distance',
         'pitch', 'yaw', 'roll', 'joy', 'anger', 'surprise', 'valence', 'fear', 'sadness', 'disgust', 'neutral', 'smile',
         'brow_raise', 'brow_furrow', 'nose_wrinkle', 'upper_lip_raise', 'mouth_open', 'eye_closure', 'cheek_raise', 'yawn',
         'blink', 'blink_rate', 'eye_widen', 'inner_brow_raise', 'lip_corner_depressor'
         ]
-
+ 
 measurements_dict = defaultdict()
 expressions_dict = defaultdict()
 emotions_dict = defaultdict()
 bounding_box_dict = defaultdict()
 time_metrics_dict = defaultdict()
-
-
-
+num_faces = defaultdict()
+ 
+ 
+ 
 class Listener(af.ImageListener):
     """
     Listener class that return metrics for processed frames.
-
+ 
     """
     def __init__(self):
         super(Listener, self).__init__()
-
+ 
     def results_updated(self, faces, image):
         global process_last_ts
         timestamp = time_metrics_dict['timestamp']
@@ -83,24 +84,27 @@ class Listener(af.ImageListener):
                                                 face.get_bounding_box()[1].x,
                                                 face.get_bounding_box()[1].y,
                                                 face.get_confidence()]
-
+ 
     def image_captured(self, image):
         global capture_last_ts
-        capture_fps = 1000.0 / (image.timestamp() - capture_last_ts)
+        try:
+            capture_fps = 1000.0 / (image.timestamp() - capture_last_ts)
+        except:
+            capture_fps = 0
         time_metrics_dict['cfps'] = capture_fps
         capture_last_ts = image.timestamp()
-
-
-
+ 
+ 
+ 
 def get_command_line_parameters(parser, args):
     """
     read parameters entered on the command line.
-
+ 
         Parameters
         ----------
         args: argparse
             object of argparse module
-
+ 
         Returns
         -------
         tuple of str values
@@ -130,18 +134,18 @@ def get_command_line_parameters(parser, args):
     frame_width = int(args.res[WIDTH])
     frame_height= int(args.res[HEIGHT])
     return input_file, data, max_num_of_faces, csv_file, output_file, frame_width, frame_height
-
-
-
+ 
+ 
+ 
 def draw_bounding_box(frame):
     """
     For each frame, draw the bounding box on screen.
-
+ 
         Parameters
         ----------
         frame: affvisionPy.Frame
             Frame object to draw the bounding box on.
-
+ 
     """
     for fid in bounding_box_dict.keys():
         upper_left_x, upper_left_y, lower_right_x, lower_right_y = get_bounding_box_points(fid)
@@ -158,18 +162,18 @@ def draw_bounding_box(frame):
             cv2.rectangle(frame, (upper_left_x, upper_left_y), (lower_right_x, lower_right_y), (0, 255, 0), 3)
         else:
             cv2.rectangle(frame, (upper_left_x, upper_left_y), (lower_right_x, lower_right_y), (21, 169, 167), 3)
-
-
-
+ 
+ 
+ 
 def get_bounding_box_points(fid):
     """
     Fetch upper_left_x, upper_left_y, lower_right_x, lwoer_right_y points of the bounding box.
-
+ 
         Parameters
         ----------
         fid: int
             face id of the face to get the bounding box for
-
+ 
         Returns
         -------
         tuple of int values
@@ -179,18 +183,18 @@ def get_bounding_box_points(fid):
             int(bounding_box_dict[fid][1]),
             int(bounding_box_dict[fid][2]),
             int(bounding_box_dict[fid][3]))
-
-
-
+ 
+ 
+ 
 def roundup(num):
     """
     Round up the number to the nearest 10.
-
+ 
        Parameters
        ----------
        num: int
            number to be rounded up to 10.
-
+ 
        Returns
        -------
        int
@@ -199,13 +203,13 @@ def roundup(num):
     if (num / 10.0) < 5:
         return int(math.floor(num / 10.0)) * 10
     return int(math.ceil(num / 10.0)) * 10
-
-
-
+ 
+ 
+ 
 def get_text_size(text, font, thickness):
     """
     Get the size occupied by a particular text string
-
+ 
        Parameters
        ----------
        text: str
@@ -214,7 +218,7 @@ def get_text_size(text, font, thickness):
            font size of the text string
        thickness: int
            thickness of the font
-
+ 
        Returns
        -------
        tuple of int values
@@ -222,13 +226,13 @@ def get_text_size(text, font, thickness):
     """
     text_size = cv2.getTextSize(text, font, TEXT_SIZE, thickness)
     return text_size[0][0], text_size[0][1]
-
-
-
+ 
+ 
+ 
 def display_measurements_on_screen(key, val, upper_left_y, frame, x1):
     """
     Display the measurement metrics on screen.
-
+ 
        Parameters
        ----------
        key: str
@@ -240,7 +244,7 @@ def display_measurements_on_screen(key, val, upper_left_y, frame, x1):
        frame: affvisionpy.Frame
            Frame object to write the measurement on
        x1: upper_left_x co-ordinate of the bounding box whose measurements need to be written
-
+ 
     """
     key = str(key)
     padding = 20
@@ -248,9 +252,9 @@ def display_measurements_on_screen(key, val, upper_left_y, frame, x1):
     key_text_width, key_text_height = get_text_size(key_name, cv2.FONT_HERSHEY_SIMPLEX, 1)
     val_text = str(round(val, 2))
     val_text_width, val_text_height = get_text_size(val_text, cv2.FONT_HERSHEY_SIMPLEX, 1)
-
+ 
     key_val_width = key_text_width + val_text_width
-
+ 
     cv2.putText(frame, key_name + ": ", (abs(x1 - key_val_width - PADDING_FOR_SEPARATOR), upper_left_y),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 TEXT_SIZE,
@@ -258,13 +262,13 @@ def display_measurements_on_screen(key, val, upper_left_y, frame, x1):
     cv2.putText(frame, val_text, (abs(x1 - val_text_width), upper_left_y),
                 cv2.FONT_HERSHEY_SIMPLEX, TEXT_SIZE,
                 (255, 255, 255))
-
-
-
+ 
+ 
+ 
 def display_emotions_on_screen(key, val, upper_left_y, frame, x1):
     """
     Display the emotion metrics on screen.
-
+ 
         Parameters
         ----------
         key: str
@@ -276,12 +280,12 @@ def display_emotions_on_screen(key, val, upper_left_y, frame, x1):
         frame: affvisionpy.Frame
             Frame object to write the measurement on
         x1: upper_left_x co-ordinate of the bounding box whose measurements need to be written
-
+ 
     """
     key = str(key)
     key_name = key.split(".")[1]
     key_text_width, key_text_height = get_text_size(key_name, cv2.FONT_HERSHEY_SIMPLEX, 1)
-
+ 
     val_rect_width = 120
     key_val_width = key_text_width + val_rect_width
     cv2.putText(frame, key_name + ": ", (abs(x1 - key_val_width), upper_left_y),
@@ -293,18 +297,18 @@ def display_emotions_on_screen(key, val, upper_left_y, frame, x1):
                 TEXT_SIZE,
                 (255, 255, 255), 2, cv2.LINE_AA)
     overlay = frame.copy()
-
+ 
     if math.isnan(val):
         val = 0
-
+ 
     start_box_point_x = abs(x1 - val_rect_width)
     width = 8
     height = 10
-
+ 
     rounded_val = roundup(val)
     rounded_val /= 10
     rounded_val = abs(int(rounded_val))
-
+ 
     for i in range(0, rounded_val):
         start_box_point_x += 10
         cv2.rectangle(overlay, (start_box_point_x, upper_left_y),
@@ -319,16 +323,16 @@ def display_emotions_on_screen(key, val, upper_left_y, frame, x1):
         start_box_point_x += 10
         cv2.rectangle(overlay, (start_box_point_x, upper_left_y),
                       (start_box_point_x + width, upper_left_y - height), (186, 186, 186), -1)
-
+ 
     alpha = 0.8
     cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-
-
-
+ 
+ 
+ 
 def display_expressions_on_screen(key, val, upper_right_x, upper_right_y, frame, upper_left_y):
     """
     Display the expressions metrics on screen.
-
+ 
         Parameters
         ----------
         key: str
@@ -342,21 +346,21 @@ def display_expressions_on_screen(key, val, upper_right_x, upper_right_y, frame,
         frame: affvisionpy.Frame
             Frame object to write the measurement on
         upper_left_y: upper_left_y co-ordinate of the bounding box whose measurements need to be written
-
+ 
     """
     key = str(key)
-
+ 
     key_name = key.split(".")[1]
     val_rect_width = 120
     overlay = frame.copy()
     if math.isnan(val):
         val = 0
-
+ 
     if 'blink' not in key:
         start_box_point_x = upper_right_x
         width = 8
         height = 10
-
+ 
         rounded_val = roundup(val)
         rounded_val /= 10
         rounded_val = int(rounded_val)
@@ -370,7 +374,7 @@ def display_expressions_on_screen(key, val, upper_right_x, upper_right_y, frame,
             start_box_point_x += 10
             cv2.rectangle(overlay, (start_box_point_x, upper_right_y),
                           (start_box_point_x + width, upper_right_y - height), (186, 186, 186), -1)
-
+ 
         alpha = 0.8
         cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
         upper_left_y += 25
@@ -379,25 +383,25 @@ def display_expressions_on_screen(key, val, upper_right_x, upper_right_y, frame,
                     (0, 0, 0), 2, cv2.LINE_AA)
         cv2.putText(frame, str(val), (upper_right_x, upper_right_y), cv2.FONT_HERSHEY_DUPLEX, TEXT_SIZE,
                     (255, 255, 255), 1, cv2.LINE_AA)
-
+ 
     cv2.putText(frame, " :" + str(key_name), (upper_right_x + val_rect_width, upper_right_y), cv2.FONT_HERSHEY_DUPLEX,
                 TEXT_SIZE,
                 (0, 0, 0), 4, cv2.LINE_AA)
     cv2.putText(frame, " :" + str(key_name), (upper_right_x + val_rect_width, upper_right_y), cv2.FONT_HERSHEY_DUPLEX,
                 TEXT_SIZE,
                 (255, 255, 255), 1, cv2.LINE_AA)
-
-
-
+ 
+ 
+ 
 def write_metrics(frame):
     """
     write measurements, emotions, expressions on screen
-
+ 
         Parameters
         ----------
         frame: affvisionpy.Frame
             frame to write the metrics on
-
+ 
     """
     for fid in measurements_dict.keys():
         measurements = measurements_dict[fid]
@@ -408,48 +412,49 @@ def write_metrics(frame):
         box_width = lower_right_x - upper_left_x
         upper_right_x = upper_left_x + box_width
         upper_right_y = upper_left_y
-
+ 
         for key, val in measurements.items():
             display_measurements_on_screen(key, val, upper_left_y, frame, upper_left_x)
-
+ 
             upper_left_y += 25
-
+ 
         for key, val in emotions.items():
             display_emotions_on_screen(key, val, upper_left_y, frame, upper_left_x)
             upper_left_y += 25
-
+ 
         for key, val in expressions.items():
             display_expressions_on_screen(key, val, upper_right_x, upper_right_y, frame, upper_left_y)
-
+ 
             upper_right_y += 25
-
-
-
+ 
+ 
+ 
 def run(csv_data):
     """
     Starting point of the program, initializes the detector, processes a frame and then writes metrics to frame
-
+ 
         Parameters
         ----------
         csv_data: list
             Values to hold for each frame
     """
+    global num_faces
     parser, args = parse_command_line()
     input_file, data, max_num_of_faces, csv_file, output_file, frame_width, frame_height = get_command_line_parameters(parser, args)
     if isinstance(input_file, int):
         start_time = time.time()
     detector = af.SyncFrameDetector(data, max_num_of_faces)
-
+ 
     detector.enable_features({af.Feature.expressions, af.Feature.emotions})
-
+ 
     list = Listener()
     detector.set_image_listener(list)
-
+ 
     detector.start()
-
+ 
     captureFile = cv2.VideoCapture(input_file)
     window = cv2.namedWindow('Processed Frame', cv2.WINDOW_NORMAL)
-
+ 
     if not args.video:
         cv2.resizeWindow('Processed Frame', frame_width, frame_height)
         captureFile.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
@@ -462,65 +467,71 @@ def run(csv_data):
             captureFile.set(cv2.CAP_PROP_FRAME_WIDTH, DEFAULT_FRAME_WIDTH)
             frame_width = DEFAULT_FRAME_WIDTH
             frame_height = DEFAULT_FRAME_HEIGHT
-
+ 
         file_width = frame_width
         file_height = frame_height
-
+ 
     else:
         file_width = int(captureFile.get(3))
         file_height = int(captureFile.get(4))
         cv2.resizeWindow('Processed Frame', file_width, file_height)
-
+ 
     if output_file is not None:
        out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (file_width, file_height))
     count = 0
     timestamp = 0
-
+    last_timestamp = 0
+ 
     while captureFile.isOpened():
         # Capture frame-by-frame
         ret, frame = captureFile.read()
-
+ 
         if ret == True:
-            last_timestamp = timestamp
+             
             height = frame.shape[0]
             width = frame.shape[1]
             if isinstance(input_file, int):
                 timestamp = (time.time() - start_time) * 1000.0
             else:
                 timestamp = int(captureFile.get(cv2.CAP_PROP_POS_MSEC))
-            time_metrics_dict['timestamp'] = timestamp #.put(timestamp)
-            afframe = af.Frame(width, height, frame, af.ColorFormat.bgr, int(timestamp))
-            count += 1
-            try:
-                if timestamp>last_timestamp and count > 0: # if there's a problem with the timestamp, don't process the frame
+            if timestamp>last_timestamp or count == 0: # if there's a problem with the timestamp, don't process the frame
+             
+                last_timestamp = timestamp
+                time_metrics_dict['timestamp'] = timestamp #.put(timestamp)
+                afframe = af.Frame(width, height, frame, af.ColorFormat.bgr, int(timestamp))
+                count += 1
+                  
+                try:
                     detector.process(afframe)
-
-            except Exception as exp:
-                print(exp)
-            write_metrics_to_csv_data_list(csv_data, round(timestamp, 0))
-
-            if len(num_faces) > 0 and not check_bounding_box_outside(width, height):
-                draw_bounding_box(frame)
-                draw_affectiva_logo(frame, width, height)
-                write_metrics(frame)
-                cv2.imshow('Processed Frame', frame)
+ 
+                except Exception as exp:
+                    print(exp)
+                write_metrics_to_csv_data_list(csv_data, round(timestamp, 0))
+ 
+                if len(num_faces) > 0 and not check_bounding_box_outside(width, height):
+                    draw_bounding_box(frame)
+                    draw_affectiva_logo(frame, width, height)
+                    write_metrics(frame)
+                    cv2.imshow('Processed Frame', frame)
+                else:
+                    draw_affectiva_logo(frame, width, height)
+                    cv2.imshow('Processed Frame', frame)
+                if output_file is not None:
+                    out.write(frame)
+ 
+                clear_all_dictionaries()
+ 
+                if cv2.waitKey(1) == 27:
+                    break
             else:
-                draw_affectiva_logo(frame, width, height)
-                cv2.imshow('Processed Frame', frame)
-            if output_file is not None:
-                out.write(frame)
-
-            clear_all_dictionaries()
-
-            if cv2.waitKey(1) == 27:
-                break
+                print("skipped a frame due to the timestamp not incrementing - old timestamp %f, new timestamp %f" % (last_timestamp,timestamp))
         else:
             break
-
+ 
     captureFile.release()
     cv2.destroyAllWindows()
     detector.stop()
-
+ 
     # If video file is provided as an input
     if not isinstance(input_file, int):
         if csv_file == DEFAULT_FILE_NAME:
@@ -531,9 +542,9 @@ def run(csv_data):
     else:
         if not csv_file == DEFAULT_FILE_NAME:
             write_csv_data_to_file(csv_data, csv_file)
-
-
-
+ 
+ 
+ 
 def clear_all_dictionaries():
     """
     Clears the dictionary values
@@ -542,13 +553,13 @@ def clear_all_dictionaries():
     emotions_dict.clear()
     expressions_dict.clear()
     measurements_dict.clear()
-
-
-
+ 
+ 
+ 
 def draw_affectiva_logo(frame, width, height):
     """
     Place logo on the screen
-
+ 
         Parameters
         ----------
         frame: affvisionpy.Frame
@@ -562,7 +573,7 @@ def draw_affectiva_logo(frame, width, height):
     logo_width = int(width / 3)
     logo_height = int(height / 10)
     logo = cv2.resize(logo, (logo_width, logo_height))
-
+ 
     y1, y2 = 0, logo_height
     x1, x2 = width - logo_width, width
     # Remove the white background from the logo so that only the word "Affectiva" is visible on screen
@@ -571,20 +582,20 @@ def draw_affectiva_logo(frame, width, height):
         color = logo[0:logo_height, 0:logo_width, c] * (1.0 - alpha)
         beta = frame[y1:y2, x1:x2, c] * (alpha)
         frame[y1:y2, x1:x2, c] = color + beta
-
-
-
+ 
+ 
+ 
 def check_bounding_box_outside(width, height):
     """
     Check if bounding box values are going outside the screen in case of face going outside
-
+ 
         Parameters
         ----------
         width: int
            width of the frame
         height: int
            height of the frame
-
+ 
     Returns
     -------
     boolean: indicating if the bounding box is outside the frame or not
@@ -594,20 +605,20 @@ def check_bounding_box_outside(width, height):
         if upper_left_x < 0 or lower_right_x > width or upper_left_y < 0 or lower_right_y > height:
             return True
         return False
-
-
-
+ 
+ 
+ 
 def write_metrics_to_csv_data_list(csv_data, timestamp):
     """
     Write metrics per frame to a list
-
+ 
         Parameters
         ----------
         csv_data:
           list of per frame values to write to
         timestamp: int
            timestamp of each frame
-
+ 
     """
     global header_row
     if not measurements_dict.keys():
@@ -634,13 +645,13 @@ def write_metrics_to_csv_data_list(csv_data, timestamp):
                 current_frame_data[str(key).split('.')[1]] = round(val,4)
             current_frame_data["confidence"] = round(bounding_box_dict[fid][4],4)
             csv_data.append(current_frame_data)
-
-
-
+ 
+ 
+ 
 def parse_command_line():
     """
     Make the options for command line
-
+ 
     Returns
     -------
     args: argparse object of the command line
@@ -662,13 +673,13 @@ def parse_command_line():
     parser.add_argument("-r", "--resolution", dest='res', metavar=('width', 'height'), nargs=2, default=[1280, 720], help="resolution in pixels (2-values): width height")
     args = parser.parse_args()
     return parser, args
-
-
-
+ 
+ 
+ 
 def write_csv_data_to_file(csv_data, csv_file):
     """
     Place logo on the screen
-
+ 
         Parameters
         ----------
         csv_data: list
@@ -685,9 +696,9 @@ def write_csv_data_to_file(csv_data, csv_file):
         writer.writeheader()
         for row in csv_data:
             writer.writerow(row)
-
+ 
     c_file.close()
-
+ 
 if __name__ == "__main__":
     csv_data = list()
     run(csv_data)
